7ce7c325db12e6427fb886514ff4ed8ee3337769
Federico <viceconti.federico@gmail.com>
2022-04-04 14:52:17 +0200

    Update project: - adding CardOrientation.none on card_orientation.dart - update kotlin and gradle version - migrate android example

--- example/android/app/src/main/kotlin/com/federico/flutter_card/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/federico/flutter_card/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.federico.flutter_card
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity;
-import io.flutter.embedding.engine.FlutterEngine;
-import io.flutter.plugins.GeneratedPluginRegistrant;
 
-class MainActivity: FlutterActivity() {
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
-    }
-}
+class MainActivity: FlutterActivity()
